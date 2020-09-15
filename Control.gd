0d669bc26d8aa4bec390c318f617cb2df3bd0c5a
Rakka Rage <rakkarage@gmail.com>
2020-09-15 19:56:24 -0400

    get_rect().has_point

--- Control.gd
+++ Control.gd
@@ -1,5 +1,9 @@
 extends Control
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.pressed and event.doubleclick:
-		print("DoubleClick")
+	if event is InputEventMouseButton and not event.pressed:
+		print("single")
+	elif event is InputEventMouseButton and event.pressed and event.doubleclick:
+		if get_rect().has_point(event.global_position):
+			print(event.as_text())
+
